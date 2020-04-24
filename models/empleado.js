bb645324590c78b2273b3ca9e5860bf22e48496d
ahuanay <76231561@continental.edu.pe>
2020-04-24 18:55:02 -0500

    update empleado y usuario

--- models/empleado.js
+++ models/empleado.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const EmpleadoSchema = new Schema({
-    dni: { type: String, required: true },
-    apellidoPaterno: { type: String, required: true },
-    apellidoMaterno: { type: String, required: true },
+    tipo_documento: { type: String, required: true },
+    numero_documento: { type: String, required: true },
+    apellido_paterno: { type: String, required: true },
+    apellido_materno: { type: String, required: true },
     nombres: { type: String, required: true },
-    telefono: { type: String, required: true },
+    sexo: { type: String, required: true },
+    numero_celular: { type: String, required: true },
     direccion: { type: String, required: true },
 });
 
--- models/usuario.js
+++ models/usuario.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UsuarioSchema = new Schema({
-    userName: { type: String, required: true },
-    userPass: { type: String, required: true },
-    rol: { type: String, required: true },
-    idEmpleado: { type: Schema.Types.ObjectId, ref: 'empleado' },
+    email: { type: String, required: true },
+    user_name: { type: String, required: true },
+    user_pass: { type: String, required: true },
+    avatar: { type: String, required: true },
+    estado: { type: Boolean, required: true },
+    empleado_id: { type: Schema.Types.ObjectId, ref: 'empleado' },
 });
 
 module.exports = mongoose.model('usuario', UsuarioSchema);
