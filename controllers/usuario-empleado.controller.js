005c2902d9802314588570454662f795fcc1ab0d
ahuanay <76231561@continental.edu.pe>
2020-04-24 19:25:05 -0500

    update

--- controllers/usuario-empleado.controller.js
+++ controllers/usuario-empleado.controller.js
@@ -13,10 +13,12 @@ controller.createUsuarioEmpleado = async (req, res) => {
     await empleadoModels.save();
 
     const usuario = {
-        userName: req.body.nombres,
-        userPass: req.body.nombres,
-        rol: req.body.nombres,
-        idEmpleado: empleadoModels._id
+        email: req.body.email,
+        user_name: req.body.user_name,
+        user_pass: req.body.user_pass,
+        avatar: req.body.avatar,
+        estado: req.body.estado,
+        empleado_id: empleadoModels._id
     }
     
     const usuarioModels = new UsuarioModels(usuario);
