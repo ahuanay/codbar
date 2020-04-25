c26424ae2ae5d8f050a372d9bb6ac2ac02a54bf6
ahuanay <76231561@continental.edu.pe>
2020-04-25 11:43:51 -0500

    modificado modelo usuario

--- models/usuario.js
+++ models/usuario.js
@@ -3,8 +3,7 @@ const { Schema } = mongoose;
 
 const UsuarioSchema = new Schema({
     email: { type: String, required: true },
-    user_name: { type: String, required: true },
-    user_pass: { type: String, required: true },
+    password: { type: String, required: true },
     avatar: { type: String, required: true },
     estado: { type: Boolean, required: true },
     empleado_id: { type: Schema.Types.ObjectId, ref: 'empleado' },
