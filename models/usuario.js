852536ffda9be5ea5fd5634cea973a1616b43b2b
ahuanay <76231561@continental.edu.pe>
2020-04-30 10:13:39 -0500

    modificado campo avatar

--- models/usuario.js
+++ models/usuario.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const UsuarioSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
-    avatar: { type: String, required: true },
+    avatar_url: { type: String, required: true },
     estado: { type: Boolean, required: true },
     rol_id: { type: Schema.Types.ObjectId, ref: 'rol' },
 });
