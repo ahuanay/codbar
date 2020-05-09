9226168e666b784ce15e8036260b7476a2bd82a1
ahuanay <76231561@continental.edu.pe>
2020-05-08 20:09:28 -0500

    init

--- controllers/modelo.controller.js
+++ controllers/modelo.controller.js
@@ -19,12 +19,13 @@ controller.getActiveModelo = async (req, res) => {
 }
 
 controller.createModelo = async (req, res) => {
-    res.json(req.file)
-    // const modelo = {
-    //     nombre: req.body.nombre,
-    //     imagen_url: 'http://' + req.headers.host + '/images/producto/'+req.file.filename,
-    //     estado: req.body.estado == 'true' ? true : false
-    // }
+    // res.json(req.file)
+    const modelo = {
+        nombre: req.body.nombre,
+        imagen_url: 'http://' + req.headers.host + '/images/producto/'+req.file.filename,
+        estado: req.body.estado == 'true' ? true : false
+    }
+    res.json(modelo)
     // const modeloModels = new ModeloModels(modelo);
     // await modeloModels.save();
     // res.status(201).json(modeloModels); 
